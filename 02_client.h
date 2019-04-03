7ca8608dcde421221eb42cba8225edf289b98b0e
Petr Karnakov <pkarnakov@gmail.com>
2019-04-03 13:57:02 +0200

    add 02_client.h

---
+++ 02_client.h
@@ -0,0 +1,40 @@
+// Examples of client code for possible support
+
+class M;             // mesh
+using Scal = double; // scalar
+class Vect;          // vector
+class IdxCell;       // cell index
+class IdxFace;       // face index
+template <class T, class Idx>   // field
+class Field;
+
+
+Scal getTotalVolume(const M& m) {
+    Scal s(0);
+    for (IdxCell c : m.Cells()) {
+        s += m.getVolume(c);
+    }
+    return s;
+}
+
+Vect getFirstMoment(const Field<Scal, IdxCell>& fc, const M& m) {
+    Vect x(0);
+    for (IdxCell c : m.Cells()) {
+        x += m.getCenter(x) * (fc[c] * m.getVolume(x));
+    }
+    return x;
+}
+
+void interpolate(const Field<Scal, IdxFace>& ff,
+                 Field<Scal, IdxCell>& fc,
+                 const M& m) {
+    fc.Init(m);
+    for (IdxCell c : m.Cells()) {
+        fc[c] = 0;
+        for (size_t i = 0; i < m.getNumFaces(c); ++i) {
+            IdxFace f = m.getFace(c, i);
+            fc[c] += ff[f];
+        }
+        fc[c] /= m.getNumFaces(c);
+    }
+}
