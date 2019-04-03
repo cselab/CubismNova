771d270121265a0c214660cdce86a323c007f6c9
Petr Karnakov <kpetr@ethz.ch>
2019-04-03 14:20:05 +0200

    02_client: add interpolate cell to face

--- 02_client.h
+++ 02_client.h
@@ -11,7 +11,7 @@ class Field;
 
 Scal getTotalVolume(const M& m) {
     Scal s(0);
-    for (IdxCell c : m.Cells()) {
+    for (auto c : m.Cells()) {
         s += m.getVolume(c);
     }
     return s;
@@ -19,7 +19,7 @@ Scal getTotalVolume(const M& m) {
 
 Vect getFirstMoment(const Field<Scal, IdxCell>& fc, const M& m) {
     Vect x(0);
-    for (IdxCell c : m.Cells()) {
+    for (auto c : m.Cells()) {
         x += m.getCenter(x) * (fc[c] * m.getVolume(x));
     }
     return x;
@@ -29,12 +29,26 @@ void interpolate(const Field<Scal, IdxFace>& ff,
                  Field<Scal, IdxCell>& fc,
                  const M& m) {
     fc.Init(m);
-    for (IdxCell c : m.Cells()) {
+    for (auto c : m.Cells()) {
         fc[c] = 0;
         for (size_t i = 0; i < m.getNumFaces(c); ++i) {
             IdxFace f = m.getFace(c, i);
             fc[c] += ff[f];
         }
         fc[c] /= m.getNumFaces(c);
+    }
+}
+
+void interpolate(const Field<Scal, IdxCell>& fc,
+                 Field<Scal, IdxFace>& ff,
+                 const M& m) {
+    ff.Init(m);
+    for (auto f : m.Faces()) {
+        ff[f] = 0;
+        for (size_t i = 0; i < m.getNumCells(f); ++i) {
+            IdxCell c = m.getCell(f, i);
+            ff[f] += fc[c];
+        }
+        ff[f] /= m.getNumCells(f);
     }
 }
