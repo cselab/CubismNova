81469b135dfe06204cb298328ec0f20ae82c5269
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2019-05-03 20:35:18 +0200

    test: add sandbox for testing code and tryouts

---
+++ test/sandbox/Fields/BlockField/TestBlockField.cpp
@@ -0,0 +1,12 @@
+// File       : TestBlockField.cpp
+// Created    : Sat Apr 13 2019 10:45:37 PM (+0200)
+// Author     : Fabian Wermelinger
+// Description: BlockField indexing
+// Copyright 2019 ETH Zurich. All Rights Reserved.
+extern void blockIndexing();
+
+int main(void)
+{
+    blockIndexing();
+    return 0;
+}
