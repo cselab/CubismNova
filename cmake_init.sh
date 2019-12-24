fc20de811f2b3bd2ff5981242a845401720ca3d6
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2019-12-24 15:56:30 +0100

    Ignore x86_64 install directory

--- cmake_init.sh
+++ cmake_init.sh
@@ -9,7 +9,8 @@ set -e
 # 1. no arguments defaults to debug
 # 2. first argument given is used as build path without debug flags
 # 3. remaining arguments are passed to cmake
-COMPILER='-DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpic++'
+ROOT="$(pwd -P)"
+COMPILER="-DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpic++ -DCMAKE_INSTALL_PREFIX=${ROOT}/x86_64"
 BUILD=debug
 OPT="-DCMAKE_BUILD_TYPE=Debug"
 if [[ $# -gt 0 ]]; then
@@ -17,7 +18,6 @@ if [[ $# -gt 0 ]]; then
     OPT="-DCMAKE_BUILD_TYPE=RelWithDebInfo"
 fi
 
-ROOT="$(pwd -P)"
 rm -rf ${BUILD}
 mkdir -p ${BUILD}
 (cd ${BUILD}; cmake ${ROOT} ${OPT} ${COMPILER} "$@")
