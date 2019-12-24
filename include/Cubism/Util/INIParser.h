6853ebd2c587e07a64ae055380b5280556efad8b
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2019-12-24 16:01:25 +0100

    Add .ini config file parser

---
+++ include/Cubism/Util/INIParser.h
@@ -0,0 +1,190 @@
+// File       : INIParser.h
+// Created    : Mon Dec 23 2019 09:39:13 PM (+0100)
+// Author     : Fabian Wermelinger
+// Description: .ini file parser.  This is a modified class based on
+//              third/inih/cpp/INIReader.h
+// Copyright 2019 ETH Zurich. All Rights Reserved.
+#ifndef INIPARSER_H_1WLRUC2B
+#define INIPARSER_H_1WLRUC2B
+
+#include "Core/Common.h"
+
+#include <map>
+#include <ostream>
+#include <queue>
+#include <string>
+#include <vector>
+
+NAMESPACE_BEGIN(Cubism)
+NAMESPACE_BEGIN(Util)
+
+class INIParser
+{
+public:
+    /// @brief Construct INIParser and parse given filename. See ini.h for more
+    /// info about the parsing.
+    ///
+    /// @param filename
+    explicit INIParser(const std::string &filename);
+
+    /// @brief Construct INIParser and parse given buffer. See ini.h for more
+    /// info about the parsing.
+    ///
+    /// @param buffer
+    /// @param buffer_size
+    explicit INIParser(const char *buffer, size_t buffer_size);
+
+    /// @brief Return the global result of ini_parse() for all files.
+    ///
+    /// @return false if no errors occurred during parsing
+    bool parseError() const;
+
+    /// @brief Return the result of ini_parse().
+    ///
+    /// @return 0 on success, line number of first error or -1 on file open
+    /// error for each file
+    std::map<std::string, int> fileErrors() const;
+
+    /// @brief Get a string value from INI file.  Throws runtime error if not
+    /// found.
+    ///
+    /// @param section
+    /// @param name
+    ///
+    /// @return string value
+    std::string get(const std::string &section, const std::string &name) const;
+
+    /// @brief Get a string value from INI file.  Throws runtime error if not
+    /// found.
+    ///
+    /// @param section
+    /// @param name
+    ///
+    /// @return parsed string
+    std::string getString(const std::string &section,
+                          const std::string &name) const;
+
+    /// @brief Get an array of string values from INI file.  Throws runtime
+    /// error if not found.
+    ///
+    /// @param section
+    /// @param name
+    ///
+    /// @return parsed array of strings
+    std::vector<std::string> getStringArray(const std::string &section,
+                                            const std::string &name) const;
+
+    /// @brief Get an integer (long) value from INI file.  Valid integers are
+    /// decimal "1234", "-1234", or hex "0x4d2".  Throws runtime error if not
+    /// found.
+    ///
+    /// @param section
+    /// @param name
+    ///
+    /// @return parsed integer
+    long getInteger(const std::string &section, const std::string &name) const;
+
+    /// @brief Get an array of integer (long) values from INI file.  Valid
+    /// integers are decimal "1234", "-1234", or hex "0x4d2".  Throws runtime
+    /// error if not found.
+    ///
+    /// @param section
+    /// @param name
+    ///
+    /// @return parsed array of integer
+    std::vector<long> getIntegerArray(const std::string &section,
+                                      const std::string &name) const;
+
+    /// @brief Get a real (floating point double) value from INI file. Valid
+    /// floating point values according to strtod().  Throws runtime error if
+    /// not found.
+    ///
+    /// @param section
+    /// @param name
+    ///
+    /// @return parsed floating point value
+    double getReal(const std::string &section, const std::string &name) const;
+
+    /// @brief Get an array of real (floating point double) values from INI
+    /// file. Valid floating point values according to strtod().  Throws runtime
+    /// error if not found.
+    ///
+    /// @param section
+    /// @param name
+    ///
+    /// @return parsed array of floating point values
+    std::vector<double> getRealArray(const std::string &section,
+                                     const std::string &name) const;
+
+    /// @brief Get a boolean value from INI file.  Valid true values are "true",
+    /// "yes", "on", "1", and valid false values are "false", "no", "off", "0"
+    /// (not case sensitive).  Throws runtime error if not found.
+    ///
+    /// @param section
+    /// @param name
+    ///
+    /// @return parsed boolean value
+    bool getBoolean(const std::string &section, const std::string &name) const;
+
+    /// @brief Get an array of boolean values from INI file.  Valid true values
+    /// are "true", "yes", "on", "1", and valid false values are "false", "no",
+    /// "off", "0" (not case sensitive).  Throws runtime error if not found.
+    ///
+    /// @param section
+    /// @param name
+    ///
+    /// @return parsed array of boolean values
+    std::vector<bool> getBooleanArray(const std::string &section,
+                                      const std::string &name) const;
+
+    /// @brief Return true if the given section exists (section must contain at
+    /// least one name=value pair).
+    ///
+    /// @param section
+    bool hasSection(const std::string &section) const;
+
+    /// @brief Return true if a value exists with the given section and field
+    /// names.
+    ///
+    /// @param section
+    /// @param name
+    bool hasValue(const std::string &section, const std::string &name) const;
+
+    friend std::ostream &operator<<(std::ostream &os, const INIParser &p);
+
+private:
+    std::queue<std::string> fqueue_;    // file queue for [include] files
+    std::map<std::string, int> ferror_; // file error map
+    std::map<std::string, std::string> values_; // raw values
+
+    /// @brief Key generator for values_ map
+    ///
+    /// @param section
+    /// @param name
+    static std::string makeKey(const std::string &section,
+                               const std::string &name);
+
+    /// @brief This implementation of value handler does not append multiple
+    /// section/value pairs but overwrite existing ones.
+    ///
+    /// @param user
+    /// @param section
+    /// @param name
+    /// @param value
+    static int valueHandler(void *user,
+                            const char *section,
+                            const char *name,
+                            const char *value);
+
+    /// @brief Handle include sections in config files in a depth-first manner.
+    /// The included files are processed in the order they appear in the file,
+    /// overwriting existing keys.
+    void recursiveIncludes_(const std::string &filename);
+};
+
+std::ostream &operator<<(std::ostream &os, const INIParser &p);
+
+NAMESPACE_END(Util)
+NAMESPACE_END(Cubism)
+
+#endif /* INIPARSER_H_1WLRUC2B */
---
+++ src/Util/INIParser.cpp
@@ -0,0 +1,283 @@
+// File       : INIParser.cpp
+// Created    : Mon Dec 23 2019 10:20:51 PM (+0100)
+// Author     : Fabian Wermelinger
+// Description: .ini file parser implementation.
+// Copyright 2019 ETH Zurich. All Rights Reserved.
+#include "Util/INIParser.h"
+#include "third/inih/ini.h"
+
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <sstream>
+#include <stdexcept>
+
+NAMESPACE_BEGIN(Cubism)
+NAMESPACE_BEGIN(Util)
+
+INIParser::INIParser(const std::string &filename)
+{
+    recursiveIncludes_(filename);
+}
+
+INIParser::INIParser(const char *buffer, size_t buffer_size)
+{
+    std::string content(buffer, buffer_size);
+    ferror_["fbuffer"] = ini_parse_string(content.c_str(), valueHandler, this);
+}
+
+bool INIParser::parseError() const
+{
+    bool err = false;
+    for (const auto &f : ferror_) {
+        err = f.second;
+    }
+    return err;
+}
+
+std::map<std::string, int> INIParser::fileErrors() const { return ferror_; }
+
+std::string INIParser::get(const std::string &section,
+                           const std::string &name) const
+{
+    std::string key = makeKey(section, name);
+    if (!values_.count(key)) {
+        throw std::runtime_error("get: key=" + name + " in section=" + section +
+                                 " does not exist");
+    }
+
+    std::string val = values_.find(key)->second;
+    if (val.find_first_not_of(" \t\n\v\f\r") == std::string::npos) {
+        throw std::runtime_error("get: key=" + name + " in section=" + section +
+                                 " has no value");
+    }
+    return val;
+}
+
+std::string INIParser::getString(const std::string &section,
+                                 const std::string &name) const
+{
+    const std::string str = get(section, name);
+    return str;
+}
+
+std::vector<std::string>
+INIParser::getStringArray(const std::string &section,
+                          const std::string &name) const
+{
+    std::vector<std::string> ret;
+    std::istringstream iss(get(section, name));
+    for (std::string val; iss >> val;) {
+        ret.push_back(val);
+    }
+    if (ret.empty()) {
+        throw std::runtime_error("getStringArray: Empty container");
+    }
+    return ret;
+}
+
+long INIParser::getInteger(const std::string &section,
+                           const std::string &name) const
+{
+    const std::string valstr = get(section, name);
+    const char *value = valstr.c_str();
+    char *end;
+    // This parses "1234" (decimal) and also "0x4D2" (hex)
+    long n = strtol(value, &end, 0);
+    if (end <= value) {
+        throw std::runtime_error("getInteger: Unexpected form. Got '" + valstr +
+                                 "'");
+    }
+    return n;
+}
+
+std::vector<long> INIParser::getIntegerArray(const std::string &section,
+                                             const std::string &name) const
+{
+    std::vector<long> ret;
+    std::istringstream iss(get(section, name));
+    for (std::string valstr; iss >> valstr;) {
+        const char *value = valstr.c_str();
+        char *end;
+        // This parses "1234" (decimal) and also "0x4D2" (hex)
+        long n = strtol(value, &end, 0);
+        if (end <= value) {
+            throw std::runtime_error("getIntegerArray: Unexpected form. Got '" +
+                                     valstr + "'");
+        }
+        ret.push_back(n);
+    }
+    if (ret.empty()) {
+        throw std::runtime_error("getIntegerArray: Empty container");
+    }
+    return ret;
+}
+
+double INIParser::getReal(const std::string &section,
+                          const std::string &name) const
+{
+    const std::string valstr = get(section, name);
+    const char *value = valstr.c_str();
+    char *end;
+    double n = strtod(value, &end);
+    if (end <= value) {
+        throw std::runtime_error("getReal: Unexpected form");
+    }
+    return n;
+}
+
+std::vector<double> INIParser::getRealArray(const std::string &section,
+                                            const std::string &name) const
+{
+    std::vector<double> ret;
+    std::istringstream iss(get(section, name));
+    for (std::string valstr; iss >> valstr;) {
+        const char *value = valstr.c_str();
+        char *end;
+        double n = strtod(value, &end);
+        if (end <= value) {
+            throw std::runtime_error("getRealArray: Unexpected form");
+        }
+        ret.push_back(n);
+    }
+    if (ret.empty()) {
+        throw std::runtime_error("getRealArray: Empty container");
+    }
+    return ret;
+}
+
+bool INIParser::getBoolean(const std::string &section,
+                           const std::string &name) const
+{
+    std::string valstr = get(section, name);
+    // Convert to lower case to make std::string comparisons case-insensitive
+    std::transform(valstr.begin(), valstr.end(), valstr.begin(), ::tolower);
+    if (valstr == "true" || valstr == "yes" || valstr == "on" ||
+        valstr == "1") {
+        return true;
+    } else if (valstr == "false" || valstr == "no" || valstr == "off" ||
+               valstr == "0") {
+        return false;
+    } else {
+        throw std::runtime_error("getBoolean: Can not parse key=" + name +
+                                 " in section=" + section + ". Got '" + valstr +
+                                 "'");
+    }
+}
+
+std::vector<bool> INIParser::getBooleanArray(const std::string &section,
+                                             const std::string &name) const
+{
+    std::string valstr = get(section, name);
+    // Convert to lower case to make std::string comparisons case-insensitive
+    std::transform(valstr.begin(), valstr.end(), valstr.begin(), ::tolower);
+    std::istringstream iss(valstr);
+    std::vector<bool> ret;
+    for (std::string val; iss >> val;) {
+        bool bval;
+        if (val == "true" || val == "yes" || val == "on" || val == "1") {
+            bval = true;
+        } else if (val == "false" || val == "no" || val == "off" ||
+                   val == "0") {
+            bval = false;
+        } else {
+            throw std::runtime_error(
+                "getBooleanArray: Can not parse key=" + name +
+                " in section=" + section + ". Got '" + val + "'");
+        }
+        ret.push_back(bval);
+    }
+    if (ret.empty()) {
+        throw std::runtime_error("getBooleanArray: Empty container");
+    }
+    return ret;
+}
+
+bool INIParser::hasSection(const std::string &section) const
+{
+    const std::string key = makeKey(section, "");
+    std::map<std::string, std::string>::const_iterator pos =
+        values_.lower_bound(key);
+    if (pos == values_.end()) {
+        return false;
+    }
+    // Does the key at the lower_bound pos start with "section"?
+    return pos->first.compare(0, key.length(), key) == 0;
+}
+
+bool INIParser::hasValue(const std::string &section,
+                         const std::string &name) const
+{
+    const std::string key = makeKey(section, name);
+    return values_.count(key);
+}
+
+std::string INIParser::makeKey(const std::string &section,
+                               const std::string &name)
+{
+    std::string key = section + "=" + name;
+    // // Convert to lower case to make section/name lookups case-insensitive
+    // std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+    return key;
+}
+
+int INIParser::valueHandler(void *user,
+                            const char *section,
+                            const char *name,
+                            const char *value)
+{
+    if (!name) // Happens when INI_CALL_HANDLER_ON_NEW_SECTION enabled
+        return 1;
+    INIParser *parser = static_cast<INIParser *>(user);
+
+    // append [include] section to file queue
+    // XXX: [fabianw@mavt.ethz.ch; 2019-12-24] THIS IS NOT THREAD SAFE!
+    std::string sec(section);
+    std::transform(sec.begin(), sec.end(), sec.begin(), ::tolower);
+    if (sec == "include" && value) {
+        parser->fqueue_.push(value);
+        // append existing key.
+        const std::string key = makeKey(sec, name);
+        if (parser->values_.count(key)) {
+            parser->values_[key] += " | ";
+        }
+        parser->values_[key] += value;
+    } else {
+        // overwrite existing key.
+        const std::string key = makeKey(section, name);
+        parser->values_[key] = value ? value : "";
+    }
+    return 1;
+}
+
+void INIParser::recursiveIncludes_(const std::string &filename)
+{
+    const auto it = ferror_.find(filename);
+    if (it != ferror_.end()) {
+        throw std::runtime_error(
+            "recursiveIncludes_: Cyclic inclusion of file '" + filename + "'");
+    }
+    ferror_[filename] = ini_parse(filename.c_str(), valueHandler, this);
+    if (fqueue_.empty()) {
+        return;
+    } else {
+        const std::string fnext = fqueue_.front();
+        fqueue_.pop();
+        recursiveIncludes_(fnext);
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, const INIParser &p)
+{
+    for (const auto &item : p.values_) {
+        std::istringstream iss(item.first);
+        std::string section, name;
+        std::getline(iss, section, '=');
+        std::getline(iss, name, '=');
+        os << "[" << section << "]: " << name << " = " << item.second << '\n';
+    }
+    return os;
+}
+
+NAMESPACE_END(Util)
+NAMESPACE_END(Cubism)
