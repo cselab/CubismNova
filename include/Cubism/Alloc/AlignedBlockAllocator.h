a7913afee8fa9753389e6a88af0ed918b4594bd6
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2019-12-29 21:23:59 +0100

    Move BlockAllocator to Alloc/AlignedBlockAllocator

--- include/Cubism/Alloc/AlignedBlockAllocator.h
+++ include/Cubism/Alloc/AlignedBlockAllocator.h
@@ -1,11 +1,10 @@
-// File       : BlockAllocator.h
+// File       : AlignedBlockAllocator.h
 // Created    : Sat Apr 13 2019 05:44:18 PM (+0200)
 // Author     : Fabian Wermelinger
-// Description: Various block allocator types for use with Base/BlockField.h
-//              types
+// Description: POSIX aligned block allocator
 // Copyright 2019 ETH Zurich. All Rights Reserved.
-#ifndef BLOCKALLOCATOR_H_W0YCMRLB
-#define BLOCKALLOCATOR_H_W0YCMRLB
+#ifndef ALIGNEDBLOCKALLOCATOR_H_ARTMHJKM
+#define ALIGNEDBLOCKALLOCATOR_H_ARTMHJKM
 
 #include "Core/Common.h"
 
@@ -17,31 +16,20 @@ NAMESPACE_BEGIN(Cubism)
 /// @brief Simple aligned memory block allocator
 ///
 /// @tparam T Data type of single block element
-/// @tparam DIMX Number of block data elements in X dimension
-/// @tparam DIMY Number of block data elements in Y dimension
-/// @tparam DIMZ Number of block data elements in Z dimension
 /// @tparam ALIGNAT Align at byte boundary
-template <typename T,
-          size_t DIMX,
-          size_t DIMY,
-          size_t DIMZ,
-          size_t ALIGNAT = 32>
+template <typename T, size_t ALIGNAT = 32>
 class AlignedBlockAllocator
 {
 public:
     using DataType = T;
-    static constexpr size_t BlockDimX = DIMX;
-    static constexpr size_t BlockDimY = DIMY;
-    static constexpr size_t BlockDimZ = DIMZ;
 
     /// @brief Allocate block memory
     ///
     /// @param nblocks Number of blocks
     ///
     /// @return Pointer to first block element
-    DataType *allocate(size_t nblocks) const
+    DataType *allocate(const size_t bytes) const
     {
-        const size_t bytes = getBytes(nblocks);
         void *block = nullptr;
         posix_memalign(&block, ALIGNAT, bytes);
         assert(block != nullptr && "posix_memalign returned NULL address");
@@ -58,18 +46,8 @@ public:
             block = nullptr;
         }
     }
-
-    /// @brief Memory footprint occupied by block(s)
-    ///
-    /// @param nblocks Number of blocks
-    ///
-    /// @return Total block memory footprint in bytes
-    size_t getBytes(size_t nblocks) const
-    {
-        return nblocks * BlockDimX * BlockDimY * BlockDimZ * sizeof(DataType);
-    }
 };
 
 NAMESPACE_END(Cubism)
 
-#endif /* BLOCKALLOCATOR_H_W0YCMRLB */
+#endif /* ALIGNEDBLOCKALLOCATOR_H_ARTMHJKM */
