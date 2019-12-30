1f032769f9a0bc52310fa084a9dd7b03a383c3bd
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2019-12-30 21:31:18 +0100

    Add block data carrier class

---
+++ include/Cubism/Block/Data.h
@@ -0,0 +1,353 @@
+// File       : Data.h
+// Created    : Sun Dec 29 2019 01:26:35 PM (+0100)
+// Author     : Fabian Wermelinger
+// Description: Basic block data
+// Copyright 2019 ETH Zurich. All Rights Reserved.
+#ifndef DATA_H_W5KVJG9U
+#define DATA_H_W5KVJG9U
+
+#include "Alloc/AlignedBlockAllocator.h"
+#include "Core/Common.h"
+#include "Core/Index.h"
+
+#include <cassert>
+#include <cstring>
+#include <type_traits>
+
+NAMESPACE_BEGIN(Cubism)
+NAMESPACE_BEGIN(Block)
+
+/// @brief Type-less base class for a data block
+class DataBase
+{
+public:
+    DataBase() = default;
+    virtual ~DataBase() = default;
+
+    DataBase(const DataBase &c) = delete;
+    DataBase(DataBase &&c) = delete;
+
+    DataBase &operator=(const DataBase &c) = delete;
+    DataBase &operator=(DataBase &&c) = delete;
+
+    /// @brief Mutable pointer to address of first data element
+    ///
+    /// @return
+    virtual void *getBlockPtr() { return nullptr; }
+
+    /// @brief Immutable pointer to address of first data element
+    ///
+    /// @return
+    virtual const void *getBlockPtr() const { return nullptr; }
+
+    /// @brief Number of bytes occupied by block
+    ///
+    /// @return Number of bytes
+    virtual size_t getBlockBytes() const { return 0; }
+
+    /// @brief Number of bytes occupied by a single data element
+    ///
+    /// @return Number of bytes
+    virtual size_t getDataElementBytes() const { return 0; }
+
+    /// @brief Number of data elements contained in block
+    ///
+    /// @return Number of block data elements
+    virtual size_t getBlockSize() const { return 0; }
+};
+
+/// @brief Generic block data
+///
+/// @tparam T Data type
+/// @tparam DM Data-mapping type
+/// @tparam DIM Data dimensionality
+/// @tparam BlockAlloc Allocator type
+template <typename T,
+          Cubism::DataMapping DM,
+          size_t DIM,
+          typename BlockAlloc = AlignedBlockAllocator<T>>
+class Data : public DataBase
+{
+public:
+    using BaseType = DataBase;
+    using AllocType = BlockAlloc;
+    using IndexRangeType = IndexRange<DIM>;
+    using MultiIndex = typename IndexRangeType::MultiIndex;
+    using DataType = typename BlockAlloc::DataType;
+    static_assert(std::is_same<DataType, T>::value,
+                  "Block allocator data type does not match type T");
+
+    enum class MemoryOwner { No = 0, Yes };
+
+    static constexpr Cubism::DataMapping DataMapping = DM;
+
+    /// @brief Base constructor for single block allocation
+    ///
+    /// @param r Index range of data (defines spatial dimensionality of data)
+    /// @param owner Whether the data is owner by the class
+    explicit Data(const IndexRangeType &r)
+        : range_(r), owner_(MemoryOwner::Yes), block_(nullptr), bytes_(0)
+    {
+        if (static_cast<bool>(owner_)) {
+            allocBlock_();
+            clearBlock_(); // NUMA touch
+        }
+    }
+
+    /// @brief General purpose copy-constructor mainly to create data views.
+    ///
+    /// @param c Right hand side
+    /// @param owner Memory ownership.  MemoryOwner::No for views
+    Data(const Data &c, const MemoryOwner owner)
+        : range_(c.range_), owner_(owner), block_(nullptr), bytes_(0)
+    {
+        if (static_cast<bool>(owner_)) {
+            allocBlock_();
+            copyBlockDeep_(c); // NUMA touched
+        } else {
+            bytes_ = c.bytes_; // explicitly set here
+            copyBlockShallow_(c);
+        }
+    }
+
+    /// @brief Low-level data view constructor, never owns data.
+    ///
+    /// @param r Index range of data pointed to by ptr
+    /// @param ptr Block data pointer to first element
+    /// @param bytes Number of bytes of block data.  This may be larger than the
+    ///              minimum required data specified by the index range!
+    Data(const IndexRangeType &r, DataType *ptr, const size_t bytes)
+        : range_(r), owner_(MemoryOwner::No), block_(ptr), bytes_(bytes)
+    {
+    }
+
+    /// @brief Copy constructor. Depending on ownership of memory, copy is deep
+    ///        or shallow. Relevant for views.
+    ///
+    /// @param c Right hand side
+    Data(const Data &c)
+        : range_(c.range_), owner_(c.owner_), block_(nullptr), bytes_(0)
+    {
+        if (static_cast<bool>(owner_)) {
+            allocBlock_();
+            copyBlockDeep_(c); // NUMA touched
+        } else {
+            bytes_ = c.bytes_; // explicitly set here
+            copyBlockShallow_(c);
+        }
+    }
+
+    /// @brief Move constructor. Move constructions are always shallow.
+    ///        Ownership of the memory is inherited from the source.
+    ///
+    /// @param c Right hand side
+    Data(Data &&c) noexcept
+        : range_(std::move(c.range_)), owner_(c.owner_), block_(nullptr),
+          bytes_(0)
+    {
+        bytes_ = c.bytes_; // explicitly set here
+        copyBlockShallow_(c);
+        c.setNull_(); // ensure that destructor of c has no effect
+    }
+
+    ~Data() override
+    {
+        if (static_cast<bool>(owner_)) {
+            deallocBlock_();
+        }
+    }
+
+    // Virtual interface of base class
+    void *getBlockPtr() override { return static_cast<void *>(block_); }
+    const void *getBlockPtr() const override
+    {
+        return static_cast<const void *>(block_);
+    }
+    size_t getBlockBytes() const override { return bytes_; }
+    size_t getDataElementBytes() const override { return sizeof(DataType); }
+    size_t getBlockSize() const override { return range_.size(); }
+
+    /// @brief Returns pointer to first data element
+    DataType *getData() { return block_; }
+
+    /// @brief Returns const pointer to first data element
+    const DataType *getData() const { return block_; }
+
+    /// @brief Copy assignment operator. Depending on ownership of memory, copy
+    ///        is deep or shallow. Relevant for views.
+    Data &operator=(const Data &c)
+    {
+        if (this != &c) {
+            if (static_cast<bool>(owner_)) {
+                copyBlockDeep_(c);
+            } else {
+                copyBlockShallow_(c);
+            }
+        }
+        return *this;
+    }
+
+    /// @brief Move assignment operator
+    Data &operator=(Data &&c)
+    {
+        if (this != &c) {
+            if (static_cast<bool>(owner_)) {
+                deallocBlock_();
+                bytes_ = c.bytes_; // explicitly set here
+            }
+            copyBlockShallow_(c);
+            c.setNull_(); // ensure that destructor of c has no effect
+        }
+        return *this;
+    }
+
+    /// @brief Linear access of data
+    DataType &operator[](size_t i)
+    {
+        assert(i < this->getBlockSize() && "Linear index out of bounds");
+        return block_[i];
+    }
+
+    /// @brief Linear access of data
+    const DataType &operator[](size_t i) const
+    {
+        assert(i < this->getBlockSize() && "Linear index out of bounds");
+        return block_[i];
+    }
+
+    /// @brief Access operator for cases DIM <= 3
+    DataType &operator()(size_t ix, size_t iy = 0, size_t iz = 0)
+    {
+        assert(ix < range_.sizeDim(0) && "Block X-index out of bounds");
+        if (1 == DIM) {
+            return block_[ix];
+        } else if (2 == DIM) {
+            assert(iy < range_.sizeDim(1) && "Block Y-index out of bounds");
+            return block_[ix + range_.sizeDim(0) * iy];
+        } else if (3 == DIM) {
+            assert(iz < range_.sizeDim(2) && "Block Z-index out of bounds");
+            return block_[ix +
+                          range_.sizeDim(0) * (iy + range_.sizeDim(1) * iz)];
+        } else {
+            throw std::runtime_error(
+                "Data::operator(): You can not call this method for DIM > 3");
+        }
+    }
+
+    /// @brief Access operator for cases DIM <= 3
+    const DataType &operator()(size_t ix, size_t iy = 0, size_t iz = 0) const
+    {
+        assert(ix < range_.sizeDim(0) && "Block X-index out of bounds");
+        if (1 == DIM) {
+            return block_[ix];
+        } else if (2 == DIM) {
+            assert(iy < range_.sizeDim(1) && "Block Y-index out of bounds");
+            return block_[ix + range_.sizeDim(0) * iy];
+        } else if (3 == DIM) {
+            assert(iz < range_.sizeDim(2) && "Block Z-index out of bounds");
+            return block_[ix +
+                          range_.sizeDim(0) * (iy + range_.sizeDim(1) * iz)];
+        } else {
+            throw std::runtime_error(
+                "Data::operator(): You can not call this method for DIM > 3");
+        }
+    }
+
+    /// @brief General multi-index access operator
+    DataType &operator()(const MultiIndex &p)
+    {
+        return this->operator[](range_.getFlatIndex(p));
+    }
+
+    /// @brief General multi-index access operator
+    const DataType &operator()(const MultiIndex &p) const
+    {
+        return this->operator[](range_.getFlatIndex(p));
+    }
+
+    /// @brief Returns index range that spans the block data
+    IndexRangeType getIndexRange() const { return range_; }
+
+    /// @brief Returns true if the block memory is owned by this instance
+    bool isMemoryOwner() const { return static_cast<bool>(owner_); }
+
+protected:
+    const IndexRangeType range_;  // range of DIM-dimensional data
+    const MemoryOwner owner_;     // owns memory allocation
+    DataType *block_;             // pointer to first block element
+    size_t bytes_;                // number of bytes pointed to by block_
+    BlockAlloc blk_alloc_;        // block allocator
+
+    /// @brief Allocate single block memory
+    ///
+    /// @return True if success
+    bool allocBlock_()
+    {
+        bytes_ = range_.size() * sizeof(DataType);
+        block_ = blk_alloc_.allocate(bytes_);
+        assert(block_ != nullptr &&
+               "Invalid memory address for block allocation");
+        if (block_ == nullptr) {
+            return false;
+        }
+        return true;
+    }
+
+    /// @brief Deallocate single block memory
+    void deallocBlock_()
+    {
+        if (block_ != nullptr) {
+            blk_alloc_.deallocate(block_);
+            block_ = nullptr;
+            bytes_ = 0;
+        }
+    }
+
+    /// @brief Set all addresses to NULL
+    void setNull_()
+    {
+        block_ = nullptr;
+        bytes_ = 0;
+    }
+
+    /// @brief Deep copy of an external source
+    ///
+    /// @param rhs Data block to be copied
+    void copyBlockDeep_(const Data &rhs)
+    {
+        static_assert(std::is_pod<DataType>::value, "DataType is not POD");
+        assert(bytes_ == rhs.bytes_ &&
+               "copyBlockDeep: Number of block bytes are not identical");
+        if (block_ != rhs.block_) {
+            std::memcpy(block_, rhs.block_, bytes_);
+        }
+    }
+
+    /// @brief Shallow copy of an external source
+    ///
+    /// @param rhs Data block to be copied
+    void copyBlockShallow_(const Data &rhs)
+    {
+        assert(bytes_ == rhs.bytes_ &&
+               "copyBlockShallow: Number of block bytes are not identical");
+        block_ = rhs.block_;
+        bytes_ = rhs.bytes_;
+    }
+
+    /// @brief Clear block memory bitwise
+    void clearBlock_()
+    {
+        static_assert(std::is_pod<DataType>::value, "DataType is not POD");
+        if (block_ != nullptr) {
+            std::memset(block_, 0, bytes_);
+        }
+    }
+};
+
+template <typename T, Cubism::DataMapping DM, size_t DIM, typename BlockAlloc>
+constexpr Cubism::DataMapping Data<T, DM, DIM, BlockAlloc>::DataMapping;
+
+NAMESPACE_END(Block)
+NAMESPACE_END(Cubism)
+
+#endif /* DATA_H_W5KVJG9U */
---
+++ test/UnitTests/Block/DataTest.cpp
@@ -0,0 +1,140 @@
+// File       : DataTest.cpp
+// Created    : Mon Dec 30 2019 06:03:31 PM (+0100)
+// Author     : Fabian Wermelinger
+// Description: Basic block data test for various allocator types
+// Copyright 2019 ETH Zurich. All Rights Reserved.
+#include "Block/Data.h"
+#include "Core/Index.h"
+#include "gtest/gtest.h"
+
+#include "Alloc/AlignedBlockAllocator.h"
+
+#include <utility>
+
+namespace
+{
+using namespace Cubism;
+
+template <typename T, typename TAlloc, size_t DIM>
+using CellData = Block::Data<T, DataMapping::Cell, DIM, TAlloc>;
+template <typename T, typename TAlloc, size_t DIM>
+using NodeData = Block::Data<T, DataMapping::Node, DIM, TAlloc>;
+
+template <typename Alloc, size_t DIM>
+void runTest()
+{
+    using IRange = IndexRange<DIM>;
+    using MIndex = typename IRange::MultiIndex;
+
+    using CData = CellData<typename Alloc::DataType, Alloc, DIM>;
+    using NData = NodeData<typename Alloc::DataType, Alloc, DIM>;
+
+    MIndex cells(16);
+    MIndex nodes(16 + 1);
+    IRange cell_domain(cells);
+    IRange node_domain(nodes);
+
+    CData cdata(cell_domain);
+    NData ndata(node_domain);
+
+    EXPECT_TRUE(cdata.getIndexRange() == cell_domain);
+    EXPECT_EQ(cdata.getDataElementBytes(), sizeof(typename Alloc::DataType));
+
+    EXPECT_EQ(cdata.getBlockSize(), cells.prod());
+    EXPECT_EQ(ndata.getBlockSize(), nodes.prod());
+
+    // data view
+    CData cdata_view(cdata, CData::MemoryOwner::No);
+    EXPECT_FALSE(cdata_view.isMemoryOwner());
+    EXPECT_TRUE(cdata.isMemoryOwner());
+    EXPECT_EQ(cdata.getBlockPtr(), cdata_view.getBlockPtr());
+
+    // copy construction
+    NData ndata_copy(ndata);
+    CData cdata_view1(cdata_view);
+    EXPECT_NE(ndata.getBlockPtr(), ndata_copy.getBlockPtr());
+    EXPECT_EQ(cdata_view1.getBlockPtr(), cdata_view.getBlockPtr());
+
+    // copy assignment
+    CData cdata1(cell_domain);
+    cdata1 = cdata; // deep copy
+    cdata_view = cdata_view1; // shallow copy
+    EXPECT_NE(cdata.getBlockPtr(), cdata1.getBlockPtr());
+    EXPECT_EQ(cdata_view1.getBlockPtr(), cdata_view.getBlockPtr());
+
+    // move construction
+    CData cdata_move(std::move(cdata));
+    EXPECT_EQ(cdata_view.getBlockPtr(), cdata_move.getBlockPtr());
+    EXPECT_NE(cdata_move.getBlockPtr(), cdata.getBlockPtr());
+
+    // move assignment
+    NData ndata_view(ndata, NData::MemoryOwner::No);
+    NData ndata_move(ndata); // deep copy
+    EXPECT_NE(ndata_view.getBlockPtr(), ndata_move.getBlockPtr());
+    ndata_move = std::move(ndata);
+    EXPECT_EQ(ndata_view.getBlockPtr(), ndata_move.getBlockPtr());
+    EXPECT_NE(ndata_view.getBlockPtr(), ndata.getBlockPtr());
+
+    // data access
+    CData ref(cdata1, CData::MemoryOwner::No);
+    for (size_t i = 0; i < ref.getBlockSize(); ++i) {
+        ref[i] = static_cast<typename Alloc::DataType>(i);
+    }
+
+    CData test(cdata_move, CData::MemoryOwner::No);
+    const auto r = test.getIndexRange();
+    if (2 == DIM) { // test operator() for DIM == 2
+        for (size_t j = 0; j < r.sizeDim(1); ++j) {
+            for (size_t i = 0; i < r.sizeDim(0); ++i) {
+                const auto val =
+                    static_cast<typename Alloc::DataType>(i + r.sizeDim(0) * j);
+                test(i, j) = val;
+                EXPECT_EQ(test(i, j), val); // const access
+            }
+        }
+    } else {
+        for (size_t i = 0; i < test.getBlockSize(); ++i) {
+            const MIndex p = r.getMultiIndex(i);
+            test(p) = static_cast<typename Alloc::DataType>(i);
+        }
+    }
+    if (DIM > 3) {
+        EXPECT_THROW(
+            {
+                try {
+                    test(0);
+                } catch (const std::runtime_error &e) {
+                    EXPECT_STREQ("Data::operator(): You can not call this "
+                                 "method for DIM > 3",
+                                 e.what());
+                    throw;
+                }
+            },
+            std::runtime_error);
+        EXPECT_THROW(
+            {
+                try {
+                    test(0) = 0;
+                } catch (const std::runtime_error &e) {
+                    EXPECT_STREQ("Data::operator(): You can not call this "
+                                 "method for DIM > 3",
+                                 e.what());
+                    throw;
+                }
+            },
+            std::runtime_error);
+    }
+
+    for (size_t i = 0; i < ref.getBlockSize(); ++i) {
+        const MIndex p = r.getMultiIndex(i);
+        EXPECT_EQ(ref[i], test(p));
+    }
+}
+
+TEST(Data, AlignedBlockAllocator)
+{
+    runTest<AlignedBlockAllocator<float>, 1>();
+    runTest<AlignedBlockAllocator<double>, 2>();
+    runTest<AlignedBlockAllocator<int>, 4>();
+}
+} // namespace
