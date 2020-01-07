7ba23bcf533d62822fd5045454a209c191224268
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2020-01-07 22:14:47 +0100

    Add CartesianTest base (wip)

--- test/UnitTests/Grid/CartesianTest.cpp
+++ test/UnitTests/Grid/CartesianTest.cpp
@@ -4,28 +4,30 @@
 // Description: Cartesian Grid test
 // Copyright 2020 ETH Zurich. All Rights Reserved.
 #include "Grid/Cartesian.h"
+#include "Core/Index.h"
+#include "Mesh/StructuredUniform.h"
+#include "gtest/gtest.h"
 
 namespace
 {
 using namespace Cubism;
-
-template <typename T, template <typename> class TAlloc, size_t DIM>
-using CellData = Block::Data<T, EntityType::Cell, DIM, TAlloc<T>>;
 
 TEST(Cartesian, Construction)
 {
     using IRange = Core::IndexRange<3>;
     using MIndex = typename IRange::MultiIndex;
 
-    using CellField =
-        Block::Field<CellData<double, AlignedBlockAllocator, Mesh::Dim>,
-                     MyFieldState>;
-
     using Mesh = Mesh::StructuredUniform<double, IRange::Dim>;
-    using PointType = typename Mesh::PointType;
-    using Range = typename Mesh::RangeType;
 
-    const MIndex block_cells(8);
+    // using MeshHull = typename Mesh::MeshHull;
+    // using PointType = typename Mesh::PointType;
+    // using Entity = typename Mesh::EntityType;
+    // using Range = typename Mesh::RangeType;
+    using Grid = Grid::Cartesian<float, Mesh>;
+
     const MIndex nblocks(2);
+    const MIndex block_cells(8);
+    Grid g(nblocks, block_cells);
 }
+
 } // namespace
