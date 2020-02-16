e58b23777aeb931737a0452bb2db9586197c3304
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2020-02-16 23:33:23 +0100

    Add specialized DataLabLoader for 3D domains

--- include/Cubism/Block/DataLabLoader.h
+++ include/Cubism/Block/DataLabLoader.h
@@ -11,6 +11,7 @@
 #include "Cubism/Core/Stencil.h"
 #include "Cubism/Core/Vector.h"
 #include "Cubism/Math.h"
+#include <cstring>
 #include <functional>
 
 NAMESPACE_BEGIN(Cubism)
@@ -98,13 +99,160 @@ struct DataLabLoader {
     }
 };
 
-// TODO: [fabianw@mavt.ethz.ch; 2020-02-12] specialized versions (notes)
-// DataType *dst = block_data_;
-// const DataType *src = f0.getData();
-// const size_t l0 = range_.sizeDim(0);        // lab stride
-// const size_t n0 = curr_range_.sizeDim(0); // data stride
-// const size_t bytes0 = n0 * sizeof(DataType);
-// const size_t N = curr_range_.size();
+// specialization for 3D
+template <typename FieldType>
+struct DataLabLoader<FieldType, 3> {
+    using DataType = typename FieldType::DataType;
+    using StencilType = Core::Stencil<3>;
+    using IndexRangeType = typename Core::IndexRange<3>;
+    using MultiIndex = typename IndexRangeType::MultiIndex;
+    using Index = typename MultiIndex::DataType;
+    using ID2Field = std::function<FieldType &(const MultiIndex &)>;
+    using BoolVec = Core::Vector<bool, 3>;
+
+    StencilType curr_stencil;
+    IndexRangeType curr_range;
+
+    void loadInner(const FieldType &src,
+                   DataType *dst,
+                   const IndexRangeType &rmemory,
+                   const MultiIndex &offset)
+    {
+        const MultiIndex sstart = curr_stencil.getBegin();
+        const MultiIndex extent = curr_range.getExtent();
+
+        const Index sx = -sstart[0];
+        const Index sy = -sstart[1];
+        const Index ey = sy + (extent[1] / 4) * 4;
+        const Index ry = sy + extent[1] - ey;
+        const Index sz = -sstart[2];
+        const Index ez = sz + extent[2];
+
+        const MultiIndex lextent = rmemory.getExtent(); // lab extent
+        const Index lstridex = lextent[0];              // lab stride x
+        const Index lslicexy = lextent[0] * lextent[1]; // lab slice xy
+
+        DataType *pdst = dst + rmemory.getFlatIndex(offset + sstart);
+        const DataType *psrc = src.getData();
+        const Index bstridex = extent[0]; // block stride x
+        const size_t bytesx = sizeof(DataType) * bstridex;
+        for (Index iz = sz; iz < ez; ++iz) {
+            const Index szx = iz * lslicexy + sx;
+            for (Index iy = sy; iy < ey; iy += 4) {
+                DataType *dst0 = pdst + szx + (iy + 0) * lstridex;
+                DataType *dst1 = pdst + szx + (iy + 1) * lstridex;
+                DataType *dst2 = pdst + szx + (iy + 2) * lstridex;
+                DataType *dst3 = pdst + szx + (iy + 3) * lstridex;
+                std::memcpy(dst0, psrc + 0 * bstridex, bytesx);
+                std::memcpy(dst1, psrc + 1 * bstridex, bytesx);
+                std::memcpy(dst2, psrc + 2 * bstridex, bytesx);
+                std::memcpy(dst3, psrc + 3 * bstridex, bytesx);
+                psrc += 4 * bstridex;
+            }
+            if (ry > 0) {
+                for (Index iy = ey; iy < ey + ry; ++iy) {
+                    DataType *dst0 = pdst + szx + iy * lstridex;
+                    std::memcpy(dst0, psrc, bytesx);
+                    psrc += bstridex;
+                }
+            }
+        }
+    }
+
+    void loadGhosts(const MultiIndex &i0,
+                    const ID2Field &i2f,
+                    DataType *dst,
+                    const IndexRangeType &rmemory,
+                    const MultiIndex &offset,
+                    const BoolVec &periodic,
+                    const MultiIndex &skip)
+    {
+        const MultiIndex one(1);
+        const IndexRangeType nbr_range(0, 3);
+        const size_t neighbors = nbr_range.size();
+        const size_t me = neighbors / 2;
+        const MultiIndex extent = curr_range.getExtent();
+        const MultiIndex halo_extent = extent + curr_stencil.getEnd() - one;
+        const MultiIndex stencil_begin = curr_stencil.getBegin();
+        DataType *pdst = dst + rmemory.getFlatIndex(offset + stencil_begin);
+        for (size_t i = 0; i < neighbors; ++i) {
+            if (i == me) {
+                continue;
+            }
+            const MultiIndex bi{i % 3 - 1, (i / 3) % 3 - 1, (i / 9) - 1};
+
+            if ((!periodic[0] && bi[0] == skip[0]) ||
+                (!periodic[1] && bi[1] == skip[1]) ||
+                (!periodic[2] && bi[2] == skip[2])) {
+                continue;
+            }
+            if (!curr_stencil.isTensorial() &&
+                (myAbs(bi[0]) + myAbs(bi[1]) + myAbs(bi[2]) > 1)) {
+                continue;
+            }
+
+            const MultiIndex begin{
+                bi[0] < 1 ? (bi[0] < 0 ? stencil_begin[0] : 0) : extent[0],
+                bi[1] < 1 ? (bi[1] < 0 ? stencil_begin[1] : 0) : extent[1],
+                bi[2] < 1 ? (bi[2] < 0 ? stencil_begin[2] : 0) : extent[2]};
+
+            const MultiIndex end{
+                bi[0] < 1 ? (bi[0] < 0 ? 0 : extent[0]) : halo_extent[0],
+                bi[1] < 1 ? (bi[1] < 0 ? 0 : extent[1]) : halo_extent[1],
+                bi[2] < 1 ? (bi[2] < 0 ? 0 : extent[2]) : halo_extent[2]};
+
+            const auto &f = i2f(i0 + bi);
+            const MultiIndex lextent = rmemory.getExtent(); // lab extent
+            const Index lstridex = lextent[0];              // lab stride x
+            const Index lslicexy = lextent[0] * lextent[1]; // lab slice xy
+            const Index sx = begin[0] - stencil_begin[0];
+            const size_t bytesx = sizeof(DataType) * (end[0] - begin[0]);
+            if (0 == bytesx) {
+                continue;
+            }
+            for (Index iz = begin[2]; iz < end[2]; ++iz) {
+                const Index szx = (iz - stencil_begin[2]) * lslicexy + sx;
+                if ((end[1] - begin[1]) % 4 != 0) {
+                    for (Index iy = begin[1]; iy < end[1]; ++iy) {
+                        DataType *dst0 =
+                            pdst + szx + (iy - stencil_begin[1]) * lstridex;
+                        const DataType *src0 = &f(begin[0] - bi[0] * extent[0],
+                                                  iy - bi[1] * extent[1],
+                                                  iz - bi[2] * extent[2]);
+                        std::memcpy(dst0, src0, bytesx);
+                    }
+                } else {
+                    for (Index iy = begin[1]; iy < end[1]; iy += 4) {
+                        DataType *dst0 =
+                            pdst + szx + (iy + 0 - stencil_begin[1]) * lstridex;
+                        DataType *dst1 =
+                            pdst + szx + (iy + 1 - stencil_begin[1]) * lstridex;
+                        DataType *dst2 =
+                            pdst + szx + (iy + 2 - stencil_begin[1]) * lstridex;
+                        DataType *dst3 =
+                            pdst + szx + (iy + 3 - stencil_begin[1]) * lstridex;
+                        const DataType *src0 = &f(begin[0] - bi[0] * extent[0],
+                                                  iy + 0 - bi[1] * extent[1],
+                                                  iz - bi[2] * extent[2]);
+                        const DataType *src1 = &f(begin[0] - bi[0] * extent[0],
+                                                  iy + 1 - bi[1] * extent[1],
+                                                  iz - bi[2] * extent[2]);
+                        const DataType *src2 = &f(begin[0] - bi[0] * extent[0],
+                                                  iy + 2 - bi[1] * extent[1],
+                                                  iz - bi[2] * extent[2]);
+                        const DataType *src3 = &f(begin[0] - bi[0] * extent[0],
+                                                  iy + 3 - bi[1] * extent[1],
+                                                  iz - bi[2] * extent[2]);
+                        std::memcpy(dst0, src0, bytesx);
+                        std::memcpy(dst1, src1, bytesx);
+                        std::memcpy(dst2, src2, bytesx);
+                        std::memcpy(dst3, src3, bytesx);
+                    }
+                }
+            }
+        }
+    }
+};
 
 NAMESPACE_END(Block)
 NAMESPACE_END(Cubism)
