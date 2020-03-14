a7a83c5f4106447363872f24deb9b874b8e63c37
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2020-03-14 14:24:35 -0700

    Add USE_ITERATOR switch in nova.cpp kernel

--- test/sandbox/Benchmarks/Kernel/Divergence/nova/nova.cpp
+++ test/sandbox/Benchmarks/Kernel/Divergence/nova/nova.cpp
@@ -39,22 +39,12 @@ int main(int argc, char *argv[])
     double t0 = t.stop();
     const Point h = grid.getMesh().getCellSize(0);
 
+    // initial condition
     auto finit = [h](FieldType &b) {
         const DataType fac = 2 * M_PI;
         const Mesh &bm = *b.getState().mesh;
-        // auto begin = bm[EntityType::Cell].begin();
-        // auto end = bm[EntityType::Cell].end();
-        // for (auto ci = begin; ci != end; ++ci) {
-        //     const Point x = bm.getCoordsCell(*ci);
-        //     b[0][*ci] = std::sin(fac * x[0]) * std::cos(fac * x[1]) *
-        //                 std::sin(fac * x[2]);
-        //     b[1][*ci] = std::cos(fac * x[0]) * std::cos(fac * x[1]) *
-        //                 std::sin(fac * x[2]);
-        //     b[2][*ci] = std::cos(fac * x[0]) * std::cos(fac * x[1]) *
-        //                 std::sin(fac * x[2]);
         for (auto &ci : bm[EntityType::Cell]) {
             const Point x = bm.getCoordsCell(ci);
-            // const Point x = bm.getCoords(ci, EntityType::Cell);
             b[0][ci] = std::sin(fac * x[0]) * std::cos(fac * x[1]) *
                        std::sin(fac * x[2]);
             b[1][ci] = std::cos(fac * x[0]) * std::cos(fac * x[1]) *
@@ -87,11 +77,13 @@ int main(int argc, char *argv[])
     lab2.allocate(s, grid[0].getIndexRange());
 
     // process block fields
-    using IndexFunctor = typename VGrid::IndexFunctor;
     auto findex0 = grid.getIndexFunctor(0); // grid block index functor comp = 0
     auto findex1 = grid.getIndexFunctor(1); // grid block index functor comp = 1
     auto findex2 = grid.getIndexFunctor(2); // grid block index functor comp = 2
 
+#ifndef USE_ITERATOR
+    using Index = typename MIndex::DataType;
+#endif /* USE_ITERATOR */
     const DataType ih0 = 1. / h[0];
     const DataType ih1 = 1. / h[1];
     const DataType ih2 = 1. / h[2];
@@ -102,25 +94,37 @@ int main(int argc, char *argv[])
         lab1.loadData(bf.getState().block_index, findex1);
         lab2.loadData(bf.getState().block_index, findex2);
 
+        // result scalar field
         auto &sf = result[bf.getState().block_index];
 
         // process lab
+#ifdef USE_ITERATOR
         const MIndex ix{1, 0, 0};
         const MIndex iy{0, 1, 0};
         const MIndex iz{0, 0, 1};
-        // auto begin = bf.getIndexRange().begin();
-        // auto end = bf.getIndexRange().end();
-        // for (auto i = begin; i != end; ++i) {
-        //     const DataType dd0_dx = lab0[*i + ix] - lab0[*i - ix];
-        //     const DataType dd1_dy = lab1[*i + iy] - lab1[*i - iy];
-        //     const DataType dd2_dz = lab2[*i + iz] - lab2[*i - iz];
-        //     sf[*i] = 0.5 * (dd0_dx * ih0 + dd1_dy * ih1 + dd2_dz * ih2);
-        for (auto i : bf.getIndexRange()) {
+        // expensive loop overhead and element access
+        for (auto &i : bf.getIndexRange()) {
             const DataType dd0_dx = lab0[i + ix] - lab0[i - ix];
             const DataType dd1_dy = lab1[i + iy] - lab1[i - iy];
             const DataType dd2_dz = lab2[i + iz] - lab2[i - iz];
             sf[i] = 0.5 * (dd0_dx * ih0 + dd1_dy * ih1 + dd2_dz * ih2);
         }
+#else
+        const auto extent = bf.getIndexRange().getExtent();
+        // cheap and fast
+        for (Index iiz = 0; iiz < extent[2]; ++iiz)
+            for (Index iiy = 0; iiy < extent[1]; ++iiy)
+                for (Index iix = 0; iix < extent[0]; ++iix) {
+                    const DataType dd0_dx =
+                        lab0(iix + 1, iiy, iiz) - lab0(iix - 1, iiy, iiz);
+                    const DataType dd1_dy =
+                        lab1(iix, iiy + 1, iiz) - lab1(iix, iiy - 1, iiz);
+                    const DataType dd2_dz =
+                        lab2(iix, iiy, iiz + 1) - lab2(iix, iiy, iiz - 1);
+                    sf(iix, iiy, iiz) =
+                        0.5 * (dd0_dx * ih0 + dd1_dy * ih1 + dd2_dz * ih2);
+                }
+#endif /* USE_ITERATOR */
     }
     const double t1 = t.stop();
 #ifdef _DUMP_
